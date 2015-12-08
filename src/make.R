a6be521ff64abbe808012db676dd050caa8e0efb
dyusuf <dilmurat.yusuf@gmail.com>
2015-12-08 20:04:57 +0100

    make.R: new version

--- src/make.R
+++ src/make.R
@@ -1,5 +1,3 @@
-require(knitr) # required for knitting from rmd to md
-require(markdown) # required for md to html 
-knit('./rcas.Rmd', 'rcas.md') # creates md file
-markdownToHTML('rcas.md', 'rcas.html') # creates html file
-#browseURL(paste('file://', file.path(getwd(),'rcas.html'), sep='')) # open file in browser 
+args <- commandArgs(TRUE)
+library('rmarkdown')
+rmarkdown::render('rcas.Rmd')
