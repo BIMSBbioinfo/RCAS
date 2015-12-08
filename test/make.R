95f72a5a34ecf361165ff3bbb0b129e75a1a536a
dyusuf <dilmurat.yusuf@gmail.com>
2015-12-09 00:15:09 +0100

    temporary solution for report output option

--- test/make.R
+++ test/make.R
@@ -1,3 +1,12 @@
-args <- commandArgs(TRUE)
 library('rmarkdown')
-rmarkdown::render('rcas.Rmd')
+
+args <- commandArgs(TRUE)## Parse arguments (we expect the form --arg=value)
+parseArgs <- function(x) strsplit(sub("^--", "", x), "=")
+argsDF <- as.data.frame(do.call("rbind", parseArgs(args)))
+argsL <- as.list(as.character(argsDF$V2))
+names(argsL) <- argsDF$V1
+
+out_prefix = argsL$out
+out_file = paste0(c(out_prefix, "rcas.html"), collapse = '.')
+
+rmarkdown::render('rcas.Rmd', output_file = out_file)
