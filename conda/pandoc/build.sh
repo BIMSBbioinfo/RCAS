94301dcd89b1d951faa27314662d48af4837413e
Dilmurat Yusuf <dilmurat.yusuf@gmail.com>
2016-02-16 01:14:06 +0100

    conda recipe: pandoc v1.16.0.2

--- conda/pandoc/build.sh
+++ conda/pandoc/build.sh
@@ -1,27 +1,35 @@
-if [ `uname` == Linux ]; then
+#!/bin/bash
 
-    # This might be different in general. You can find the general location by
-    # running ghci and running
-    #   :m System.Directory
-    #   getAppUserDataDirectory "cabal"
-    # I couldn't figure out how to run this dynamically. ghci doesn't seem to have
-    # a -c switch like python.
-    #export PATH="$HOME/.cabal/bin:$PATH"
-    cabal install alex happy
-    cabal install --only-dependencies
-    cabal install hsb2hs  # a required build tool
-    cabal install --flags="embed_data_files" citeproc-hs
-    cabal configure --flags="embed_data_files"
-    cabal build
-    mkdir -p $PREFIX/bin
-    cp $SRC_DIR/dist/build/pandoc/pandoc $PREFIX/bin/pandoc
+#the recipe follows the instruction in
+#https://github.com/jgm/pandoc/blob/master/INSTALL
+#sections: Custom install & Creating a relocatable binary
 
-fi
+#the binary is compiled on Ubuntu 14.04 trusty
+#using ghc 7.10.1, cabal 1.22.6.0, hsb2hs-0.3.1
 
-if [ `uname` == Darwin ]; then
-    mkdir work
-    cd work
-    cpio -i -I ../Contents/Archive.pax.gz
-    mkdir $PREFIX/bin
-    cp usr/local/bin/* $PREFIX/bin/
-fi
+#to obtain ghc 7.10.1 and cabal 1.22 on Ubuntu 14.04:
+#sudo add-apt-repository ppa:hvr/ghc
+#sudo apt-get update
+#sudo apt-get install ghc-7.10.1
+#sudo apt-get install cabal-install-1.22
+#export PATH=/opt/ghc/7.10.1/bin:$PATH
+#export PATH=/opt/cabal/1.22/bin:$PATH
+
+#to obtain hsb2hs 0.3.1:
+#https://hackage.haskell.org/package/hsb2hs-0.3.1/hsb2hs-0.3.1.tar.gz
+#cabal install hsb2hs.cabal
+#export PATH=$HOME/.cabal/bin:$PATH
+
+cabal update
+cabal install --only-dependencies
+
+#Creating a relocatable binary
+#The resulting binary can be run from any directory
+#and is completely self-contained.
+cabal install hsb2hs  # required for --flags="embed_data_files"
+cabal configure --flags="embed_data_files" #embed all data files into the binary
+cabal build
+
+mkdir -p $PREFIX/bin
+
+cp $SRC_DIR/dist/build/pandoc/pandoc $PREFIX/bin/pandoc
