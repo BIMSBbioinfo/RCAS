c91d36fdaae18a681ba8fbf13040411e509e1d24
dyusuf <dilmurat.yusuf@gmail.com>
2016-02-16 17:22:48 +0100

    meme-suite conda recipe: add 2to3 in build.sh. The reason is that conda does not allow concurrent installation of py2 and py3. This causes the conflict with other python programs suh as snakemake.

--- conda/meme-suite/build.sh
+++ conda/meme-suite/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#transform py2 to py3
+#so there is no conflict beween
+2to3 -w scripts/*py
+2to3 -w scripts/*py.in
+
 ./configure --prefix=`pwd`/meme --with-url="http://meme-suite.org"
 
 make install
