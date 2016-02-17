a9cf54ad569109b1eee5fdb59b5a7749aeefceb2
Dilmurat Yusuf <dilmurat.yusuf@gmail.com>
2016-02-17 08:42:04 +0100

    meme-suite conda recipe: modify build.sh feed $PREFIX directly to --prefix.

--- conda/meme-suite/build.sh
+++ conda/meme-suite/build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 #transform py2 to py3
-#so there is no conflict beween
+#so there is no conflict, during conda installation,
+#between meme-chip and other py3 based programs in RCAS pipeline.
+#https://github.com/BIMSBbioinfo/RCAS
+#though it is an ad-hoc feature, it should not affect
+#the general use of meme-suite.
 2to3 -w scripts/*py
 2to3 -w scripts/*py.in
 
-./configure --prefix=`pwd`/meme --with-url="http://meme-suite.org"
+./configure --prefix=$PREFIX --with-url="http://meme-suite.org"
 
 make install
-
-cp -R ./meme/bin/ $PREFIX
