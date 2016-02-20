9485944f905c57d2bdc1679f297ee116fff4fc98
Dilmurat Yusuf <dilmurat.yusuf@gmail.com>
2016-02-20 09:30:07 +0100

    meme-suite conda recipe: remove 2to3 automatic translation

--- conda/meme-suite/build.sh
+++ conda/meme-suite/build.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-#transform py2 to py3
-#so there is no conflict, during conda installation,
-#between meme-chip and other py3 based programs in RCAS pipeline.
-#https://github.com/BIMSBbioinfo/RCAS
-#though it is an ad-hoc feature, it should not affect
-#the general use of meme-suite.
-2to3 -w scripts/*py
-2to3 -w scripts/*py.in
-
 ./configure --prefix=$PREFIX --with-url="http://meme-suite.org"
 
 make install
