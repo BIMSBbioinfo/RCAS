f380702069b650cbfd7a30deb0b4ba76f40644d7
borauyar <bora.uyar@mdc-berlin.de>
2016-03-15 17:40:51 +0100

    updated run.rcas.R -> added GO analysis module

--- src/run.rcas.R
+++ src/run.rcas.R
@@ -7,7 +7,7 @@
 args <- commandArgs(TRUE)
 
 ## Default setting when no arguments passed
-if(length(args) < 1) {
+if(length(args) < 3) {
   args <- c("--help")
 }
 
@@ -17,10 +17,9 @@ preprocess.anot.R: given a gtf/gff3 file; create rtracklayer::GRanges and Genomi
 Arguments:
 --gff_file=<path to ENSEMBL GTF/GFF file>  - e.g /data/akalin/Base/Annotation/GenomeAnnotation/hg19/EnsemblGenes/*.gtf
 --peak_file=<path to query regions in BED format> - e.g. Hafner2009.bed
---help              - print this text
+--genome_version=<choose genome version: e.g. hg19; dm3; ce6; mm9>
+--help              - print this text"
 
-Example:
-Rscript preprocess.anot.R --gff_file=/data/akalin/Base/Annotation/GenomeAnnotation/hg19/EnsemblGenes/142812_EnsemblFTP_hg19_EnsemblGenes.gtf"
 ## Help section
 if("--help" %in% args) {
   cat(help_command, "\n")
@@ -35,8 +34,20 @@ names(argsL) <- argsDF$V1
 
 if(!("gff_file" %in% argsDF$V1)) {
   cat(help_command, "\n")
-  stop("provide the path to gtf/gff file")
+  stop("provide the path to gtf/gff file\n")
 }
+
+if(!("peak_file" %in% argsDF$V1)) {
+  cat(help_command, "\n")
+  stop("provide the path to input BED file\n")
+}
+
+if(!("genome_version" %in% argsDF$V1)) {
+  cat(help_command, "\n")
+  stop("provide the genome version: choose between hg19, mm9, ce6 or dm3\n")
+}
+
+
 
 #load libraries#
 suppressMessages(library('tools'))
@@ -45,6 +56,26 @@ suppressMessages(library('GenomicFeatures'))
 
 gff_file = argsL$gff_file
 peak_file = argsL$peak_file
+genome_version = argsL$genome_version
+
+### must be configured by configuration script 
+source_dir = '/home/buyar/projects/RCAS/src'
+###
+
+if(!genome_version %in% c('hg19', 'ce6', 'mm9', 'dm3')){
+  cat(help_command,"\n")
+  stop("Error: The supported genome versions are hg19, ce6, mm9 and dm3\n")
+}
+
+if(genome_version %in% c('hg19', 'hg38')){
+  species = 'human'
+}else if(genome_version %in% c('mm9', 'mm10')){
+  species = 'mouse'
+}else if(genome_version %in% c('ce6', 'ce10')){
+  species = 'worm'
+}else if(genome_version %in% c('dm3', 'dm6')){
+  species = 'fly'
+}
 
 gff_version = file_ext(gff_file)
 
@@ -69,14 +100,14 @@ if(file.exists(paste0(gff_file, ".granges.rds")))
 }
 
 if(!is.null(gff)){
-  if(!file.exists(paste0(gff_file, 'gfeatures.txdb')))
+  if(!file.exists(paste0(gff_file, '.gfeatures.txdb')))
   {  
     cat('Creating txdb object from GRanges\n')
     txdb = makeTxDbFromGRanges(gr = gff)
     cat('Saving txdb into file\n')
-    saveDb(txdb, file = paste0(gff_file,'gfeatures.txdb'))
+    saveDb(txdb, file = paste0(gff_file,'.gfeatures.txdb'))
   }else{
-    cat('Txdb already exists.. Loading from ',paste0(gff_file, 'gfeatures.txdb'),'\n')
+    cat('Txdb already exists.. Loading from ',paste0(gff_file, '.gfeatures.txdb'),'\n')
   }
 }else{
   stop('imported GFF is empty!\n')
@@ -90,10 +121,28 @@ all_gene_ids = na.omit(unique(gff$gene_id))
 overlaps = gff[queryHits(findOverlaps(gff, peaks))]
 targeted_gene_ids = na.omit(unique(overlaps$gene_id))
 
-out_prefix = sub(x = basename(peak_file), pattern = file_ext(peak_file), replacement = '')
+out_prefix = sub(x = basename(peak_file), pattern = paste0('.',file_ext(peak_file)), replacement = '')
 
-write(x = all_gene_ids, file = paste0(out_prefix, '.background_genes.txt'))
-write(x = targeted_gene_ids, file = paste0(out_prefix, '.targeted_genes.txt'))
+background_geneset = paste0(out_prefix, '.background_genes.txt')
+write(x = all_gene_ids, file = background_geneset)
+targeted_geneset = paste0(out_prefix, '.targeted_genes.txt')
+write(x = targeted_gene_ids, file = targeted_geneset)
+
+#Use the gene lists to run GO term and msigdb analyses 
+GO_command = paste0('Rscript ',source_dir,'/rcas.GO.R',
+                  ' --background_list=',background_geneset,
+                  ' --targeted_list=',targeted_geneset,
+                   ' --out_prefix=',out_prefix,
+                   ' --species=',species)
+cat(GO_command,'\n')
+#system(GO_command)
+
+
+
+
+
+
+
 
 
 
