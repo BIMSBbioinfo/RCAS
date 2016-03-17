94fff2de520c1f02cbb95dc8097ac71bd82093cf
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2016-03-17 16:48:10 +0100

    configure: Check for doParallel package.

--- guix.scm
+++ guix.scm
@@ -67,6 +67,7 @@
      ("r-topgo" ,r-topgo)
      ("r-dt" ,r-dt)
      ("r-plotly" ,r-plotly)
+     ("r-doparallel" ,r-doparallel)
      ("r-motifrg" ,r-motifrg)
      ("r-genomation" ,r-genomation)
      ("r-genomicfeatures" ,r-genomicfeatures)
