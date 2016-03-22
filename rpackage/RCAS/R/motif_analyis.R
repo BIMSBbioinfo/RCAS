4986fdcc8800fbde64cd6af987a6bedb25d66caf
Uyar <buyar@AKALIN10.local>
2016-03-22 14:25:56 +0100

    updated motif_analysis.R with new functions
    - createControlRegions
    - extractSequences
    - runMotifRG
    - getMotifSummaryTable

--- rpackage/RCAS/R/motif_analyis.R
+++ rpackage/RCAS/R/motif_analyis.R
@@ -7,11 +7,11 @@ createControlRegions <- function (queryRegions) {
   flanks <- upFlank
   flanks[s==1] <- downFlank[s==1]
 
-  df <- data.frame(chr <- GenomicRanges::seqnames(queryRegions),
-                   start <- GenomicRanges::start(queryRegions),
-                   end <- GenomicRanges::end(queryRegions),
-                   strand <- GenomicRanges::strand(queryRegions),
-                   width <- GenomicRanges::width(queryRegions))
+  df <- data.frame(seqnames = GenomicRanges::seqnames(queryRegions),
+                   start    = GenomicRanges::start(queryRegions),
+                   end      = GenomicRanges::end(queryRegions),
+                   strand   = GenomicRanges::strand(queryRegions),
+                   width    = GenomicRanges::width(queryRegions))
 
   df$flankStart <- GenomicRanges::start(flanks)
   df$flankEnd <- GenomicRanges::end(flanks)
@@ -21,7 +21,7 @@ createControlRegions <- function (queryRegions) {
   controlRegions <- df[1] #control intervals
   controlRegions$start <- df$randomFlankStart
   controlRegions$end <- df$randomFlankEnd
-  controlRegions$id <- paste(controlRegions$chr, controlRegions$start, controlRegions$end, df$strand, sep=':')
+  controlRegions$id <- paste(controlRegions$seqnames, controlRegions$start, controlRegions$end, df$strand, sep=':')
   controlRegions$score <- 1
   controlRegions$strand <- df$strand
 
@@ -31,20 +31,78 @@ createControlRegions <- function (queryRegions) {
   return(controlRegions)
 }
 
-runMotifRG <- function (queryRegions, genomeVersion) {
+#' @export
+extractSequences <- function (queryRegions, genomeVersion) {
+
+  if (genomeVersion == 'hg19') {
+    seqDb = BSgenome.Hsapiens.UCSC.hg19::Hsapiens
+  } else if(genome_version == 'mm9') {
+    seqDb = BSgenome.Mmusculus.UCSC.mm9::Mmusculus
+  } else if(genome_version == 'ce6') {
+    seqDb = BSgenome.Celegans.UCSC.ce6::Celegans
+  } else if(genome_version == 'dm3') {
+    seqDb = BSgenome.Dmelanogaster.UCSC.dm3::Dmelanogaster
+  } else {
+    stop ("Cannot do motif analysis for genome versions except: hg19, mm9, ce6, and dm3\n")
+  }
+  GenomeInfoDb::seqlevelsStyle(seqDb) = 'UCSC'
+  sequences <- Biostrings::getSeq(seqDb, queryRegions)
+  names(sequences) <- paste('seq',1:length(sequences), sep='_')
+
+  return (sequences)
+}
+
+#' @export
+runMotifRG <- function (queryRegions, genomeVersion, motifN = 5) {
 
   controlRegions <- createControlRegions(queryRegions)
 
   cat('extracting peak sequences from fasta..\n')
-  peak_seqs <- getSeq(seq_fasta, peaks)
-  names(peak_seqs) <- paste('seq',1:length(peak_seqs), sep='_')
+  querySeqs <- extractSequences(queryRegions, genomeVersion)
 
   cat('extracting background sequences from fasta..\n')
-  control_seqs <- getSeq(seq_fasta, control_peaks)
-  names(control_seqs) <- paste('seq',1:length(control_seqs), sep='_')
+  controlSeqs <- extractSequences(controlRegions, genomeVersion)
 
   cat('running motifRG...')
-  motif_results <- findMotifFgBg(peak_seqs, control_seqs, enriched.only=T, max.motif=5, is.parallel=TRUE, mc.cores=8, both.strand=FALSE)
+  motifResults <- motifRG::findMotifFgBg(querySeqs, controlSeqs, enriched.only=T, max.motif=motifN , both.strand=FALSE)
+
+  return(motifResults)
 }
 
+#' @export
+getMotifSummaryTable = function(motifResults){
+
+  if(is.null(motifResults)) {return(NULL)}
+  motifs = motifResults$motifs
+  category = motifResults$category
+  scores <- c()
+  motifPatterns <- c()
+  hitsCounts1 <- c()
+  hitsCounts2 <- c()
+  seqCounts1 <- c()
+  seqCounts2 <- c()
+  fgSet <- category == 1
+  bgSet <- !fgSet
+  fgSize <- sum(fgSet)
+  bgSize <- sum(bgSet)
+  #summarize motif
+  for(i in 1:length(motifs)){
+    motifPatterns <- c(motifPatterns, motifs[[i]]@pattern)
+    scores[i] <- motifs[[i]]@score
+    count <- motifs[[i]]@count
+    hitsCounts1[i] <- sum(count[fgSet])
+    hitsCounts2[i] <- sum(count[bgSet])
+    seqCounts1[i] <-  sum(count[fgSet] > 0)
+    seqCounts2[i] <-  sum(count[bgSet] > 0)
+  }
+  ratio <- (hitsCounts1/hitsCounts2)/(fgSize/bgSize)
+  frac1 <- seqCounts1/fgSize
+  frac2 <- seqCounts2/bgSize
+  summary <- data.frame(patterns=motifPatterns,
+                        scores=round(scores,1),
+                        fgHits=hitsCounts1, bgHits= hitsCounts2,
+                        fgSeq=seqCounts1, bgSeq = seqCounts2,
+                        ratio=round(ratio,1), fgFrac=round(frac1,4), bgFrac=round(frac2,4))
+  return(summary)
+}
 
