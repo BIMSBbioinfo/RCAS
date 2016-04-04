c0fc10cb4f7f354eb8e8c5c62409bfc0225b105f
borauyar <bora.uyar@mdc-berlin.de>
2016-04-04 16:50:28 +0200

    wrote tests for functions related to GO term analysis

---
+++ rpackage/RCAS/tests/testthat/test_GO.R
@@ -0,0 +1,28 @@
+library(RCAS)
+context("Functions to do GO term enrichment analysis")
+
+data(gff)
+data(queryRegions)
+test_that("Importing test data", {
+  expect_is(gff, 'GRanges')
+  expect_is(queryRegions, 'GRanges')
+})
+
+overlaps <- queryGff(queryRegions = queryRegions, gff = gff)
+test_that("Testing queryGff function", {
+  expect_is(overlaps, 'GRanges')
+  expect_equal(length(overlaps), 12200)
+  expect_equal(length(colnames(mcols(overlaps))), 17)
+})
+
+
+goResults <- runTopGO(ontology = 'CC',
+                      species = 'human',
+                      backgroundGenes = unique(gff$gene_id),
+                      targetedGenes = unique(overlaps$gene_id))
+test_that("GO term analysis for cellular compartments", {
+  expect_is(goResults, 'data.frame')
+  expect_equal(colnames(goResults)[7], 'bonferroni')
+  expect_equal(colnames(goResults)[8], 'bh')
+})
+
