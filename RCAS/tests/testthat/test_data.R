750b17321aeccc215d402f1ed5f02f8bc2ddc3d4
borauyar <bora.uyar@mdc-berlin.de>
2016-04-16 19:53:24 +0200

    fixed namespaces in test_data.R

--- RCAS/tests/testthat/test_data.R
+++ RCAS/tests/testthat/test_data.R
@@ -5,14 +5,14 @@ data(gff)
 test_that("gff is a GRanges object containing test data of dimensions", {
   expect_is(object = gff, 'GRanges')
   expect_equal(length(gff), 238010)
-  expect_equal(ncol(mcols(gff)), 16)
+  expect_equal(ncol(GenomicRanges::mcols(gff)), 16)
 })
 
 data(queryRegions)
 test_that("queryRegions is a GRanges object from a HITS-CLIP dataset", {
   expect_is(object = queryRegions, 'GRanges')
   expect_equal(length(queryRegions), 10000)
-  expect_equal(round(mean(score(queryRegions))), 45)
+  expect_equal(round(mean(GenomicRanges::score(queryRegions))), 45)
 })
 
 data(msigDB)
