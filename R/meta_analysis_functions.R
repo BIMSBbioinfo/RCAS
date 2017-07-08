274a519a523bf20cc6bc738a48e584144293bd9f
borauyar <bora.uyar@mdc-berlin.de>
2017-07-08 11:54:39 +0200

    added new R script - meta_analysis_functions.R

---
+++ R/meta_analysis_functions.R
@@ -0,0 +1,23 @@
+#' importBedFiles
+#'
+#' This function is a wrapper that uses \code{RCAS::importBed()} function to
+#' import BED files as a GRangesList object
+#' 
+#' @param filePaths A vector of paths to one or more BED files
+#' @param ... Parameters passed to RCAS::importBed function
+#' @return A \code{GRangesList} object containing the coordinates of the
+#'   intervals from multiple input BED files
+#'   
+#' @examples
+#' input1 <- system.file("extdata", "testfile.bed", package='RCAS')
+#' input2 <- system.file("extdata", "testfile2.bed", package='RCAS')
+#' importBed(filePaths = c(input1, input2), keepStandardChr = TRUE)
+#' 
+#' @importFrom GenomicRanges GRangesList
+#' @export
+importBedFiles <- function(filePaths, ...) {
+  GenomicRanges::GRangesList(sapply(filePaths, function(f) {
+    RCAS::importBed(filePath = f, ...)
+  }, USE.NAMES = TRUE))
+}
+
