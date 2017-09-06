7bc4d2c39003b38c6c0a9d023991a2916e8df7ae
borauyar <bora.uyar@mdc-berlin.de>
2017-09-06 09:17:47 +0200

    Deleted getOverlapSimilarityMatrix function

--- R/meta_analysis_functions.R
+++ R/meta_analysis_functions.R
@@ -77,44 +77,6 @@ summarizeQueryRegionsMulti <- function(queryRegionsList, txdbFeatures, nodeN = 1
 }
 
 
-#' getOverlapSimilarityMatrix
-#' 
-#' This function takes as input a GRangesList object and finds pairwise
-#' overlapping intervals in all list elements. Then, it creates a similarity
-#' matrix based on the percentage of intervals in each GRanges object to overlap
-#' any intervals in every other GRanges object in the list.
-#' 
-#' @param queryRegionsList A GRangesList object
-#' @param nodeN (default: 2) Number of nodes to use to parallellize the process
-#' @param ... Other arguments passed to IRanges::overlapsAny function
-#' @return A matrix with equal number of rows and columns where the size is 
-#'   determined by the number of list elements in the input object. Matrix 
-#'   values are between 0 and 1, where 0 means no similarity (no overlap) and 1 
-#'   means all elements of object A overlap have at least one overlap with some 
-#'   element in B (but not necessarily vice versa).
-#' @examples 
-#' data(queryRegions)
-#' queryRegionsList <- GRangesList(queryRegions, queryRegions, 
-#'                                 queryRegions, queryRegions)
-#' getOverlapSimilarityMatrix(queryRegionsList, type = 'any')  
-#' @importFrom parallel makeCluster
-#' @importFrom parallel stopCluster
-#' @importFrom parallel clusterCall
-#' @export
-getOverlapSimilarityMatrix <- function(queryRegionsList, nodeN = 2, ...) {
-  cl <- parallel::makeCluster(nodeN)
-  parallel::clusterCall(cl, function() library(IRanges))
-  M <- parSapply(cl = cl, X = queryRegionsList, FUN = function(gr1) {
-    sapply(X = queryRegionsList, 
-           FUN = function(gr2) {
-             round(sum(overlapsAny(gr1, gr2, ...)) / length(gr1), 2)
-           })
-  })
-  parallel::stopCluster(cl)
-  return(M)
-}
-
-
 #' getIntervalOverlapMatrix
 #' 
 #' This function is used to obtain a binary matrix of overlaps between a list of
