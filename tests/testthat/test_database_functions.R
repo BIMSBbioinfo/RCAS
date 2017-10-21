bf740d838aa2db5c8e49740cad2bc1fc4ecb8671
borauyar <bora.uyar@mdc-berlin.de>
2017-10-21 20:58:02 +0200

    simplified tests in test_database_functions because tests stalled at R CMD Check

--- tests/testthat/test_database_functions.R
+++ tests/testthat/test_database_functions.R
@@ -1,52 +1,20 @@
 library(RCAS)
 context("tests for setting up and modifying sqlite database")
 
-## prepare data for tests
-if(file.exists('./test.RCASDB.sqlite')) {
-  file.remove('./test.RCASDB.sqlite')
-}
-
-if(file.exists('./myProjDataFile.tsv')){
-  file.remove('./myProjDataFile.tsv')
-}
-
-FUS_path <- system.file("extdata", "FUS_Nakaya2013c_hg19.bed",
-                        package='RCAS')
-
-FMR1_path <- system.file("extdata","FMR1_Ascano2012a_hg19.bed", 
-                         package='RCAS')
-
-projData <- data.frame('sampleName' = c('FUS', 'FMR1'), 
-                       'bedFilePath' = c(FUS_path,FMR1_path), 
-                       stringsAsFactors = FALSE)
-
-write.table(projData, 
-            'myProjDataFile.tsv', 
-            sep = '\t', quote =FALSE,
-            row.names = FALSE)
-
-gtfFilePath <- system.file("extdata", "hg19.sample.gtf", package='RCAS')
+dbPath <- system.file("extdata", "hg19.RCASDB.sqlite", package='RCAS')
+dbPathTest <- file.path(getwd(), 'test.RCASDB.sqlite')
+file.copy(from = dbPath, to = dbPathTest, overwrite = TRUE)
 
 # Testing createDB function
 test_that("Testing createDB function", {
-  expect_equal(object = createDB(dbPath = './test.RCASDB.sqlite', 
-                        projDataFile = './myProjDataFile.tsv',
-                        gtfFilePath = gtfFilePath,
+  expect_error(createDB(dbPath = dbPath,
                         genomeVersion = 'hg19',
-                        motifAnalysis = FALSE,
-                        coverageProfiles = FALSE), 
-               expected = './test.RCASDB.sqlite')
-  
-  expect_error(createDB(dbPath = './test.RCASDB.sqlite', 
-                        projDataFile = './myProjDataFile.tsv',
-                        gtfFilePath = gtfFilePath,
-                        genomeVersion = 'hg19', 
                         update = FALSE),
                regexp = 'A database already exists')
 })
 
-## Testing summarizeDatabaseContent function 
-summary <- summarizeDatabaseContent(dbPath = './test.RCASDB.sqlite')
+## Testing summarizeDatabaseContent function
+summary <- summarizeDatabaseContent(dbPath = dbPathTest)
 test_that("Testing summarizeDatabaseContent function", {
   expect_is(summary, 'data.frame')
   expect_equal(nrow(summary), 2)
@@ -54,7 +22,7 @@ test_that("Testing summarizeDatabaseContent function", {
   expect_identical(as.vector(summary$sampleName), c("FMR1", "FUS"))
 })
 
-mydb <- deleteSampleDataFromDB(dbPath = './test.RCASDB.sqlite', sampleNames = 'FUS')
+mydb <- deleteSampleDataFromDB(dbPath = dbPathTest, sampleNames = 'FUS')
 samples <- RSQLite::dbReadTable(mydb, 'processedSamples')
 
 ## Testing deleteSampleDataFromDB function
@@ -66,10 +34,6 @@ test_that("Testing deleteSampleDataFromDB function", {
 
 RSQLite::dbDisconnect(mydb)
 
-if(file.exists('./test.RCASDB.sqlite')) {
-  file.remove('./test.RCASDB.sqlite')
-}
-
-if(file.exists('./myProjDataFile.tsv')){
-  file.remove('./myProjDataFile.tsv')
+if(file.exists(dbPathTest)) {
+  file.remove(dbPathTest)
 }
