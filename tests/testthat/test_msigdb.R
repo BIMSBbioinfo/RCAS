13cf72f5205d418e8b3b24f0e37547d07489fab0
borauyar <bora.uyar@mdc-berlin.de>
2018-07-24 11:02:03 +0200

    update tests for msigdb related functions

--- tests/testthat/test_msigdb.R
+++ tests/testthat/test_msigdb.R
@@ -19,22 +19,19 @@ test_that("Gene set enrichment analysis using random gene sets", {
 })
 
 test_that("Creating biomart connections for different genome versions",{
-  expect_is(getBioMartConnection(genomeVersion = 'hg38'), 'Mart')
-  expect_is(getBioMartConnection(genomeVersion = 'hg19'), 'Mart')
-  expect_is(getBioMartConnection(genomeVersion = 'mm10'), 'Mart')
-  expect_is(getBioMartConnection(genomeVersion = 'mm9'), 'Mart')
-  expect_is(getBioMartConnection(genomeVersion = 'dm3'), 'Mart')
-  expect_is(getBioMartConnection(genomeVersion = 'ce10'), 'Mart')
-  expect_error(getBioMartConnection(genomeVersion = 'ce6'))
-  expect_error(getBioMartConnection(genomeVersion = 'bar'))
-  expect_error(getBioMartConnection(genomeVersion = 'foo'))
+  expect_is(getBioMartConnection('hsapiens'), 'Mart')
+  expect_is(getBioMartConnection('mmusculus'), 'Mart')
+  expect_is(getBioMartConnection('dmelanogaster'), 'Mart')
+  expect_is(getBioMartConnection('celegans'), 'Mart')
+  expect_error(getBioMartConnection('foo'))
+  expect_error(getBioMartConnection('bar'))
 })
 
 genes <- c('2645','5232', '5230','5162','5160')
-orth <- retrieveOrthologs(mart1 = getBioMartConnection('hg19'),
-                          mart2 = getBioMartConnection('mm9'),
+orth <- retrieveOrthologs(mart1 = getBioMartConnection('hsapiens'),
+                          mart2 = getBioMartConnection('mmusculus'),
                           geneSet = genes)
 test_that("Retrieving orthologs of a given set of genes", {
-  expect_equal(length(orth$EntrezGene.ID.1), 5)
+  expect_equal(length(orth$NCBI.gene.ID.1), 5)
 })
 
